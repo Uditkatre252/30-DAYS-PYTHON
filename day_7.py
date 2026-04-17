f9fa14249e84748ecc811ee037505f393f672111
udit <uditkatre25@gmail.com>
2026-04-17 14:24:38 +0530

    day_7 added

---
+++ day_7.py
@@ -0,0 +1,66 @@
+# Functions
+import math
+def print_name(name):
+    print(name)
+print_name('udit')
+
+def add_two_numbers(a,b):
+    return a+b
+add_two_numbers(4,6)
+
+def area_of_circle(radius):
+    return math.pi*radius**2
+area_circle = area_of_circle(5)
+print(area_circle)
+
+def add_all_nums(*args):
+    if not all(isinstance(x, (int, float)) for x in args):
+        return "All numbers should be integers."
+
+    return sum(args)
+print(add_all_nums(1,2,3,4,5,6,'t',8,9))
+
+def convert_celsius_to_fahrenheit(temp):
+    return temp*(9/5)+32
+print(convert_celsius_to_fahrenheit(10))
+
+
+def check_season(month):
+    month = month.capitalize()
+    seasons = {
+        'Winter': ['December', 'January', 'February'],
+        'Spring': ['March', 'April', 'May'],
+        'Summer': ['June', 'July', 'August'],
+        'Autumn': ['September', 'October', 'November']
+    }
+
+    for season, months in seasons.items():
+        if month in months:
+            return season
+
+    return "Invalid Month"
+
+print(check_season("march"))
+
+def calculate_slope(x1, y1, x2, y2):
+    if x2 -x1 == 0:
+        return "Undefined"
+    return (y2-y1)/(x2-x1)
+print(calculate_slope(2,3,2,5))
+
+def print_list(list):
+    return list[0:len(list)-1]
+lst = [1,2,3,4,5]
+print(print_list(lst))
+
+def reverse_list(lst):
+    return lst[::-1]
+print(reverse_list(lst))
+
+def capitalize_list_items(list):
+    return [item.capitalize() for item in list]
+
+fruits = ['apple', 'banana', 'orange']
+print(capitalize_list_items(fruits))
+
+
