03b1676fb9d734dfa23f1545f908b4c937a9d9f1
udit <uditkatre25@gmail.com>
2026-04-19 23:40:26 +0530

    updated

--- Day-09-Practice/Practice.py
+++ Day-09-Practice/Practice.py
@@ -1,35 +1,46 @@
-# # print table in time duration
-import time
-num = int(input('Enter a number: '))
-for i in range (1,11):
-    time.sleep(1)
-    print(f'{num} {i}s are {num*i}')
+# # # print table in time duration
+# import time
+# num = int(input('Enter a number: '))
+# for i in range (1,11):
+#     time.sleep(1)
+#     print(f'{num} {i}s are {num*i}')
+#
+# # print character at even index
+# string = "Geeks"
+# for i in range(0,len(string),2):
+#     print(string[i],end='')
+#
+# # # While loop printing
+# x = 3
+# while x>=0:
+#     print(x,end=" ")
+#     x = x - 1
+#
+# x = 10
+# num = 1
+# while num*num <= x:
+#     print(num*num)
+#     num += 1
+#
+# n = 9
+#
+# while n != 0:
+#     print(n)
+#     if n > 0:
+#         n -= 1
+#     else:
+#         n += 1
 
-# print character at even index
-string = "Geeks"
-for i in range(0,len(string),2):
-    print(string[i],end='')
+print('I hope everyone is enjoying the Python challenge.\nAre you')
+print('Days\tTopics\tExercises')
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35')
+print('This is backlash symbol(\\)')
+print('In every programming language it starts with \"Hello, World!\"')
 
-# # While loop printing
-x = 3
-while x>=0:
-    print(x,end=" ")
-    x = x - 1
 
-x = 10
-num = 1
-while num*num <= x:
-    print(num*num)
-    num += 1
-
-n = 9
-
-while n != 0:
-    print(n)
-    if n > 0:
-        n -= 1
-    else:
-        n += 1
 
 
 
