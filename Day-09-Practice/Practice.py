7650f13e52af90708ae90bcff5063bc064503ffc
udit <uditkatre25@gmail.com>
2026-04-19 15:41:40 +0530

    Pratice.py file updated

--- Day-09-Practice/Practice.py
+++ Day-09-Practice/Practice.py
@@ -1,20 +1,20 @@
 # # print table in time duration
-# import time
-# num = int(input('Enter a number: '))
-# for i in range (1,11):
-#     time.sleep(1)
-#     print(f'{num} {i}s are {num*i}')
+import time
+num = int(input('Enter a number: '))
+for i in range (1,11):
+    time.sleep(1)
+    print(f'{num} {i}s are {num*i}')
 
-# # print character at even index
-# string = "Geeks"
-# for i in range(0,len(string),2):
-#     print(string[i],end='')
+# print character at even index
+string = "Geeks"
+for i in range(0,len(string),2):
+    print(string[i],end='')
 
 # # While loop printing
-# x = 3
-# while x>=0:
-#     print(x,end=" ")
-#     x = x - 1
+x = 3
+while x>=0:
+    print(x,end=" ")
+    x = x - 1
 
 x = 10
 num = 1
@@ -22,10 +22,14 @@ while num*num <= x:
     print(num*num)
     num += 1
 
-x = 10
-num = 1
-while num*num <= x:
-    print(num*num)
+n = 9
+
+while n != 0:
+    print(n)
+    if n > 0:
+        n -= 1
+    else:
+        n += 1
 
 
 
