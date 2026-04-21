f4e4c53b7670c1c95c652075d375e665caadfa50
Udit katre <161617012+Uditkatre252@users.noreply.github.com>
2026-04-21 21:41:45 +0530

    Add list comprehension example in list_comprehension.py
    Demonstrate list comprehension to convert a string to a list.

---
+++ Day-11-List-comprehension/list_comprehension.py
@@ -0,0 +1,10 @@
+# One way
+language = 'Python'
+lst = list(language) # changing the string to list
+print(type(lst))     # list
+print(lst)           # ['P', 'y', 't', 'h', 'o', 'n']
+
+# Second way: list comprehension
+lst = [i for i in language]
+print(type(lst)) # list
+print(lst)       # ['P', 'y', 't', 'h', 'o', 'n']
