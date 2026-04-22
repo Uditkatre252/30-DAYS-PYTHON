4e47f070638fcda74e5362670736011eafc7e707
Uditkatre252 <uditkatre25@gmail.com>
2026-04-22 21:17:13 +0530

    list.comprehension updated

--- Day-11-List-comprehension/list_comprehension.py
+++ Day-11-List-comprehension/list_comprehension.py
@@ -1,10 +1,21 @@
-# One way
-language = 'Python'
-lst = list(language) # changing the string to list
-print(type(lst))     # list
-print(lst)           # ['P', 'y', 't', 'h', 'o', 'n']
 
-# Second way: list comprehension
+language = 'Python'
+lst = list(language)
+print(type(lst))
+print(lst)
+
+
 lst = [i for i in language]
 print(type(lst)) # list
-print(lst)       # ['P', 'y', 't', 'h', 'o', 'n']
+print(lst)
+
+# Generating numbers
+number = [i for i in range(11)]
+print(number)
+squares = [i*i for i in range(11)]
+print(number)
+number_tuple = [(i , i*i) for i in range(11)]
+print(number_tuple)
+
+even_numbers = (i for i in range(21) if i % 2 == 0)
+print(even_numbers)
