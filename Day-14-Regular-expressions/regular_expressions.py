831f3658f4815f1bca00942f56eab86d9d5e215a
Uditkatre252 <uditkatre25@gmail.com>
2026-04-25 22:58:16 +0530

    regular_expressions.py updated

--- Day-14-Regular-expressions/regular_expressions.py
+++ Day-14-Regular-expressions/regular_expressions.py
@@ -1,9 +1,19 @@
 import re
 
-txt = 'i love doing python and java'
+txt = 'i love doing python and java and Python is very easy language'
 match = re.match('i love',txt, re.I)
 print(match)
 
 span = match.span()
 print(span)
 
+txt = 'i love doing python and java'
+search = re.search('and',txt, re.I)
+print(search)
+
+matches = re.findall('and',txt, re.I)
+print(matches)
+
+txt = 'i love doing python and java and Python is very easy language'
+matches_replaced = re.sub('python|Python','javascript',txt,re.I)
+print(matches_replaced)
