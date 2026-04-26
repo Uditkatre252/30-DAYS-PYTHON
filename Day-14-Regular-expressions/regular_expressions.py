b35e447a56062de832b55818310d2362a4f97c22
Uditkatre252 <uditkatre25@gmail.com>
2026-04-26 21:43:56 +0530

    regular_expressions.py updated

--- Day-14-Regular-expressions/regular_expressions.py
+++ Day-14-Regular-expressions/regular_expressions.py
@@ -17,3 +17,10 @@ print(matches)
 txt = 'i love doing python and java and Python is very easy language'
 matches_replaced = re.sub('python|Python','javascript',txt,re.I)
 print(matches_replaced)
+
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. 
+There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;
+I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+replaced = re.sub(r'[%&@$#;]', '', sentence)
+print(replaced)
