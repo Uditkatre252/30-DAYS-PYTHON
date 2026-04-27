59e310d0b542bc86b9cd0c342c9d6db7c4d5bd89
Uditkatre252 <uditkatre25@gmail.com>
2026-04-27 23:17:02 +0530

    file_handling.py updated

--- Day-15-File-Handling/file_handling.py
+++ Day-15-File-Handling/file_handling.py
@@ -1,3 +1,16 @@
-import os
+# import os
+#
+# os.remove('C:/Users/uditk/OneDrive/Desktop/files/r_file.txt.txt')
 
-os.remove('C:/Users/uditk/OneDrive/Desktop/files/r_file.txt.txt')
+# # creating a file and overwrite if already exist
+# f = open('C:/users/uditk/onedrive/Desktop/files/udit.txt','w')
+# f.write('Hello World')
+# f.close()
+
+# read file
+w = open('C:/users/uditk/onedrive/Desktop/files/udit.txt','a+')
+w.write('Hello World\n')
+w.seek(0)
+print(w.read())
+w.close()
+
