0d45764fcbd337dc4f7f1b82eb19f5fe3db57cad
Uditkatre252 <uditkatre25@gmail.com>
2026-04-28 17:17:46 +0530

    file_handling.py updated

--- Day-15-File-Handling/file_handling.py
+++ Day-15-File-Handling/file_handling.py
@@ -1,4 +1,5 @@
-# import os
+import os
+import json
 #
 # os.remove('C:/Users/uditk/OneDrive/Desktop/files/r_file.txt.txt')
 
@@ -7,10 +8,28 @@
 # f.write('Hello World')
 # f.close()
 
-# read file
-w = open('C:/users/uditk/onedrive/Desktop/files/udit.txt','a+')
-w.write('Hello World\n')
-w.seek(0)
-print(w.read())
-w.close()
+# # read file
+# w = open('C:/users/uditk/onedrive/Desktop/files/udit.txt','a+')
+# w.write('Hello World\n')
+# w.seek(0)
+# print(w.read())
+# w.close()
+#
+# print(w)
 
+data = {
+        "name": "udit katre",
+        "email": "uditkatre7@gmail.com",
+    }
+
+data_json = json.dumps(data,indent = 4)
+# print(type(data_json))
+# print(data_json)
+data["city"] = "Bhopal"
+with open("data.json","w",encoding='utf-8') as f:
+
+    json.dump(data,f,ensure_ascii=False,indent=4)
+
+
+with open("data.json","r",encoding='utf-8') as f:
+    print(f.read())
